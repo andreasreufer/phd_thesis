d9c64a97645512a4684f03c124b7985667c32576
Andreas Reufer <andreas.reufer@space.unibe.ch>
2011-04-19 08:47:42 +0200

    changes

--- 02figs/04aneos_phases/plot.py
+++ 02figs/04aneos_phases/plot.py
@@ -14,6 +14,10 @@ from plot_helpers import *
 from const_cgs import *
 
 plt.rc('savefig', dpi=450)
+mp.rc('text', usetex=True)
+#plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble = '\usepackage{amssymb}, \usepackage{wasysym}')
+
 
 aneosf = pt.openFile("aneos_tables.hdf5")
 
@@ -27,22 +31,24 @@ aneosf = pt.openFile("aneos_tables.hdf5")
 #rhoref = 3.32
 
 matstr = "05"
-rhoref = 7.85
+#mattitle = r'\textrm{test}_2'
+mattitle = r'$\mathrm{undamaged~water~ice}$'
+rhoref = 7850
 
 UL = eval("aneosf.root.mat" + matstr + "UL")
 UH = eval("aneosf.root.mat" + matstr + "UH")
 SL = eval("aneosf.root.mat" + matstr + "SL")
 SH = eval("aneosf.root.mat" + matstr + "SH")
 
-rhomin = np.power(10., UL.log10rho[0]  )
-rhomed = np.power(10., UL.log10rho[-1] )
-rhomax = np.power(10., UH.log10rho[-1] )
+rhomin = np.power(10., UL.log10rho[0]  ) * 1.e3
+rhomed = np.power(10., UL.log10rho[-1] ) * 1.e3
+rhomax = np.power(10., UH.log10rho[-1] ) * 1.e3
 
-umin = np.power(10., UL.log10u[0]  ) / 1.e7
-umax = np.power(10., UL.log10u[-1] ) / 1.e7
+umin = np.power(10., UL.log10u[0]  ) / 1.e4
+umax = np.power(10., UL.log10u[-1] ) / 1.e4
 
-smin = np.power(10., SL.log10S[0] )  / 11605 / 1.e7
-smax = np.power(10., SL.log10S[-1] ) / 11605 / 1.e7
+smin = np.power(10., SL.log10S[0] )  / (11605 * 1.e4)
+smax = np.power(10., SL.log10S[-1] ) / (11605 * 1.e4)
 
 phsUL = UL.phase[:,:].astype(np.int8).transpose()
 phsUH = UH.phase[:,:].astype(np.int8).transpose()
@@ -68,6 +74,7 @@ phsSHC = cmap[phsSH]
 
 plt.figure( figsize=(11,6) )
 
+
 ax1L = plt.axes( np.array( [1./11.,1./6.,1./11.,4./6.] ) )
 ax1H = plt.axes( np.array( [2./11.,1./6.,3./11.,4./6.] ) )
 ax1L.loglog( 2.*rhomin, 0.1*umin, '.' )
@@ -79,14 +86,13 @@ ax1L.xaxis.set_ticks( [1.e-10, 0.1] )
 ax1H.axis([rhomed, rhomax, umin, umax])
 ax1H.yaxis.set_ticks_position("right")
 
-ax1H.set_xlabel(r"$\rho [g/cm^3]$")
-ax1L.set_ylabel(r"$u [J/g]$")
+ax1H.set_xlabel(r"$\rho [kg/m^3]$")
+ax1L.set_ylabel(r"$u [J/kg]$")
 
 ULar = (np.log10( rhomed ) - np.log10( rhomin ))/( np.log10( umax ) - np.log10( umin ))*3.
 UHar = (np.log10( rhomax ) - np.log10( rhomed ))/( np.log10( umax ) - np.log10( umin ))*1.
 ax1L.imshow(phsULC, extent=[rhomin, rhomed, umin, umax], aspect=ULar)
 ax1H.imshow(phsUHC, extent=[rhomed, rhomax, umin, umax], aspect=UHar)
-
 
 
 ax2L = plt.axes( np.array( [6./11.,1./6.,1./11.,4./6.] ) )
@@ -100,8 +106,8 @@ ax2L.xaxis.set_ticks( [1.e-10, 0.1] )
 ax2H.axis([rhomed, rhomax, smin, smax])
 ax2H.yaxis.set_ticks_position("right")
 ax2H.yaxis.set_label_position("right")
-ax2H.set_xlabel(r"$\rho [g/cm^3]$")
-ax2H.set_ylabel(r'$\mathrm{entropy} [J/g/K]$')
+ax2H.set_xlabel(r"$\rho [kg/m^3]$")
+ax2H.set_ylabel(r'$\mathrm{entropy} [J/kg/K]$')
 
 SLar = (np.log10( rhomed ) - np.log10( rhomin ))/( np.log10( smax ) - np.log10( smin ))*3.
 SHar = (np.log10( rhomax ) - np.log10( rhomed ))/( np.log10( smax ) - np.log10( smin ))*1.
@@ -114,4 +120,11 @@ for ax in axs:
   ax.yaxis.set_major_formatter(exponly_formatter)
 
 aneosf.close()
+
+bgax = plt.axes( [0., 0., 1., 1.], frameon=False)
+bgax.text( 0.4, 0.9, mattitle)
+bgax.axis([0., 1., 0., 1.])
+bgax.xaxis.set_ticks( [] )
+bgax.yaxis.set_ticks( [] )
+
 plt.savefig("out"+matstr+".png")
