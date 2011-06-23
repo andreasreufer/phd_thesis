622c2d8aeead3409ed67bf47495419355f778635
Andreas Reufer <andreas.reufer@space.unibe.ch>
2011-06-23 14:57:59 +0200

    changes

--- 03figs/18_strpeff_vimp/plot.sh
+++ 03figs/18_strpeff_vimp/plot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #rm *.pdf
 ipython -noconfirm_exit plot.py c1
-#ipython -noconfirm_exit plot.py i1
+ipython -noconfirm_exit plot_i1.py i1
 #ipython -noconfirm_exit plot.py r3
 
--- 03figs/18_strpeff_vimp/plot_func.py
+++ 03figs/18_strpeff_vimp/plot_func.py
@@ -7,49 +7,23 @@ nan = float('nan')
 
 def plotFunc(sim):
   res = sim.results
-  #return (res.mm[1] - sim.tarb.m) / sim.impb.m
-  #accreff = (res.mm[1] - sim.tarb.m) / sim.impb.m
-  #strpeff = (sim.impb.m - res.mm[2]) / sim.impb.m
-
-  #mat = 1
-  mat = 5
-
+  
   matA = 1
   matB = 5
 
-  Arelloss = ( sim.impb.mmat[matA] - res.mmatm[2,matA] ) / sim.impb.mmat[matA]
-  Brelloss = ( sim.impb.mmat[matB] - res.mmatm[2,matB] ) / sim.impb.mmat[matB]
+  Arelloss = -( sim.impb.mmat[matA] - res.mmatm[2,matA] ) / sim.impb.mmat[matA]
+  Brelloss = -( sim.impb.mmat[matB] - res.mmatm[2,matB] ) / sim.impb.mmat[matB]
   
   msr  = res.mm[2]
   mimp = sim.impb.m
 
-  if msr < 0.01*mimp or msr > 0.90*mimp:
+  if msr < 0.01*mimp:# or msr > 0.90*mimp:
     return nan
   else:
     return Arelloss - Brelloss
 
-  mimpmat = sim.impb.mmat[mat]
-  
-  frac = res.mmatm[2,mat] / res.mm[2]
-  frc0 = sim.impb.mmat[mat] / sim.impb.m
-
-  
-
-  #if res.mm[2] < 0.1*sim.impb.m:
-  if msr < 0.1*mimp or msr > 0.90*mimp:
-    return nan
-  else:
-    return frac
-    #return  res.mmatm[2,mat] / sim.impb.mmat[mat]
-
-  dfrc = (frac - frc0) / frc0
-
-  if res.mm[2] < 0.1*sim.impb.m:
-    dfrc = nan
-  return dfrc
 
 def filterFunc(sim):
-  #return sim.results.valid and ( sim.results.valtmax / sim.tcol > 5. ) and (sim.results.mm[2] > 0.1*sim.impb.m )
   return sim.results.valid and ( sim.results.valtmax / sim.tcol > 5. )
 
 def auxplot(ax):
@@ -58,10 +32,12 @@ def auxplot(ax):
 #xvar = "vimp"
 xvar = "angle"
 
-yaxis = [ -0.1, 1.0 ]
+#yaxis = [ -.30, 1.10 ]
+yaxis = [ -1.10, 0.3 ]
 ylog  = False
-ylbl  = r"$\xi$"
+ylbl  = r"$\delta_{SiO_2} - \delta_{Fe}$"
 yfmt  = math_formatter
-ytik = (0., 0.5 , 1.0 )  
+#ytik = (0., 0.5 , 1.0 )  
+ytik = (-1.0, -0.5, 0., 0.2)
 
 
