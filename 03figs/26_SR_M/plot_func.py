e5ba202fe1266ffe384ecfc8a6dc464ddb435636
Andreas Reufer <andreas.reufer@space.unibe.ch>
2011-06-25 13:36:25 +0200

    changes

---
+++ 03figs/26_SR_M/plot_func.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env ipython
+
+from plot_helpers import *
+from numpy import sqrt, pi
+
+nan = float('nan')
+
+def plotFunc(sim):
+  res = sim.results
+  if res.mm[2] > 0.05*sim.impb.m:
+    return (res.mm[2] / sim.impb.m)
+  else:
+    return nan
+
+
+def filterFunc(sim):
+  return sim.results.valid and ( sim.results.valtmax / sim.tcol > 5. ) and sim.results.mm.shape[0] > 2
+
+
+xvar = "vimp"
+#xvar = "angle"
+
+yaxis = [ -0.1, 1.1 ]
+ylog  = False
+ylbl  = r"$M_{SR} / M_{imp}$"
+yfmt  = math_formatter
+#ytik = (-2., -1., 0., 1.)
+ytik = (0., 0.2, 0.4, 0.6, 0.8, 1.0)
+
+
