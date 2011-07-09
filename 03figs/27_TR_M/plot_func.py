f2d33706803f502536ba16158c5e1fa52b625104
Andreas Reufer <andreas.reufer@space.unibe.ch>
2011-07-09 19:56:15 +0200

    changes

--- 03figs/27_TR_M/plot_func.py
+++ 03figs/27_TR_M/plot_func.py
@@ -22,13 +22,13 @@ def filterFunc(sim):
 xvar = "vimp"
 #xvar = "angle"
 
-yaxis = [ 1.e-3, 1.1 ]
+yaxis = [ 9.e-3, 1.1 ]
 ylog  = True
 #ylbl  = r"$\sum_{i = 3}^N M_i / M_{imp}$"
-ylbl  = r"$M_{TR} / M_{imp}$"
+ylbl  = r"$M_{tr} / M_{imp}$"
 yfmt  = tex_formatter
 #ytik = (-2., -1., 0., 1.)
 #ytik = (0., 0.2, 0.4, 0.6, 0.8, 1.0)
-ytik = (1.e-3, 1.e-2, 1.e-1, 1.0)
+ytik = (1.e-2, 1.e-1, 1.0)
 
 
